a50b09e129fce6ad34c9aa3121c58f443ab2df47
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-10-16 00:29:17 +0800

    refactor: organize code into structures

--- midterms/ff/first_fit.c
+++ midterms/ff/first_fit.c
@@ -1,61 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
+  int count, *sizes;
+} Object;
+
+typedef struct
+{
+  int fileID, fileSize, blockID, blockSize, transferredFileSize, remainingBlockSize;
+} Process;
+
 int getArraySize(const char *text)
 {
-  int arraySize;
-  printf("Enter the number of %s: ", text), scanf("%d", &arraySize);
-  return arraySize;
+  int count;
+  printf("Enter the number of %s: ", text), scanf("%d", &count);
+  return count;
 }
 
-int *getArrayElements(const char *sizeOfText, const char *text, int arraySize)
+int *getArrayElements(const char *sizeOfText, const char *text, int count)
 {
-  int *array = (int *)malloc(arraySize * sizeof(int));
-
+  int *array = (int *)malloc(count * sizeof(int));
   printf("\nEnter the sizes of the %s:\n", sizeOfText);
-  for (int i = 0; i < arraySize; i++)
+  for (int i = 0; i < count; i++)
     printf("%s #%d: ", text, i + 1), scanf("%d", &array[i]);
   return array;
 }
 
-void firstFit(int blocks[], int blocksSize, int files[], int filesSize)
+void firstFit(Object memory, Object file, Process *processes)
 {
-  int allocation[filesSize], transferred[filesSize];
-  for (int i = 0; i < filesSize; i++)
+  for (int i = 0; i < file.count; i++)
   {
-    allocation[i] = transferred[i] = -1;
-    for (int j = 0; j < blocksSize; j++)
-      if (blocks[j] >= files[i])
+    processes[i].fileID = i + 1;
+    processes[i].fileSize = file.sizes[i];
+
+    for (int j = 0; j < memory.count; j++)
+    {
+      if (memory.sizes[j] >= file.sizes[i])
       {
-        allocation[i] = j;
-        transferred[i] = files[i];
-        blocks[j] -= files[i];
+        processes[i].blockID = j + 1;
+        processes[i].blockSize = memory.sizes[j];
+        processes[i].transferredFileSize = file.sizes[i];
+        processes[i].remainingBlockSize = memory.sizes[j] - file.sizes[i];
+        memory.sizes[j] -= file.sizes[i];
         break;
       }
+    }
   }
+}
 
-  puts("\nFile Allocation using First-Fit:");
-  puts("File Number\tFile Size\tBlock Number\tTransferred File Size");
-  for (int i = 0; i < filesSize; i++)
+void printOutput(Object memory, Object file, Process *processes)
+{
+  printf("\n%-18s%-18s%-18s%-18s%-18s%-18s\n", "File IDs", "File Size", "Block IDs", "Block Size", "Transferred FS", "Remaining BS");
+  for (int i = 0; i < file.count; i++)
   {
-    printf("%d\t\t%d\t\t", i + 1, files[i]);
-    if (allocation[i] != -1)
-      printf("%d\t\t%d\n", allocation[i] + 1, transferred[i]);
+    printf("%-18d%-18d", processes[i].fileID, processes[i].fileSize);
+    if (processes[i].blockID == 0)
+      printf("%-18s%-18s%-18s%-18s\n", "Not Allocated", "Not Allocated", "Not Allocated", "Not Allocated");
     else
-      puts("Not Allocated\t-");
+      printf("%-18d%-18d%-18d%-18d\n", processes[i].blockID, processes[i].blockSize, processes[i].transferredFileSize, processes[i].remainingBlockSize);
   }
 }
 
 int main()
 {
-  int blocksCount = getArraySize("memory blocks");
-  int filesCount = getArraySize("files");
-  int *memoryBlocks = getArrayElements("memory blocks", "Block", blocksCount);
-  int *files = getArrayElements("files", "File", filesCount);
+  Object memory = {0, NULL}, file = {0, NULL};
+  memory.count = getArraySize("memory blocks");
+  file.count = getArraySize("files");
+  memory.sizes = getArrayElements("memory blocks", "block", memory.count);
+  file.sizes = getArrayElements("files", "file", file.count);
+  Process *processes = (Process *)malloc(file.count * sizeof(Process));
 
-  firstFit(memoryBlocks, blocksCount, files, filesCount);
-  free(memoryBlocks);
-  free(files);
+  firstFit(memory, file, processes);
+  printOutput(memory, file, processes);
 
   return 0;
 }
