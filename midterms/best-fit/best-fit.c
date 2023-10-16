bce38be3ad5f46e1dd073ef8d35aa17474117cb6
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-10-16 09:08:38 +0800

    refactor: future-proof printOutput

--- midterms/best-fit/best-fit.c
+++ midterms/best-fit/best-fit.c
@@ -27,7 +27,7 @@ int *getArrayElements(const char *sizeOfText, const char *text, int count)
   return array;
 }
 
-void bestFit(Object memory, Object file, Process *processes)
+void bestFit(Object block, Object file, Process *processes)
 {
   for (int i = 0; i < file.count; i++)
   {
@@ -35,27 +35,29 @@ void bestFit(Object memory, Object file, Process *processes)
     processes[i].fileSize = file.sizes[i];
 
     int min = __INT_MAX__;
-    for (int j = 0; j < memory.count; j++)
-      if (memory.sizes[j] >= file.sizes[i] && memory.sizes[j] < min)
-        min = memory.sizes[j];
+    for (int j = 0; j < block.count; j++)
+      if (block.sizes[j] >= file.sizes[i] && block.sizes[j] < min)
+        min = block.sizes[j];
 
-    for (int j = 0; j < memory.count; j++)
-      if (memory.sizes[j] == min)
+    for (int j = 0; j < block.count; j++)
+    {
+      if (block.sizes[j] == min)
       {
         processes[i].blockID = j + 1;
-        processes[i].blockSize = memory.sizes[j];
+        processes[i].blockSize = block.sizes[j];
         processes[i].transferredFileSize = file.sizes[i];
-        processes[i].remainingBlockSize = memory.sizes[j] - file.sizes[i];
-        memory.sizes[j] -= file.sizes[i];
+        processes[i].remainingBlockSize = block.sizes[j] - file.sizes[i];
+        block.sizes[j] -= file.sizes[i];
         break;
       }
+    }
   }
 }
 
-void printOutput(Object memory, Object file, Process *processes)
+void printOutput(int processCount, Process *processes)
 {
   printf("\n%-18s%-18s%-18s%-18s%-18s%-18s\n", "File IDs", "File Size", "Block IDs", "Block Size", "Transferred FS", "Remaining BS");
-  for (int i = 0; i < file.count; i++)
+  for (int i = 0; i < processCount; i++)
   {
     printf("%-18d%-18d", processes[i].fileID, processes[i].fileSize);
     if (processes[i].blockID == 0)
@@ -67,15 +69,18 @@ void printOutput(Object memory, Object file, Process *processes)
 
 int main()
 {
-  Object memory = {0, NULL}, file = {0, NULL};
-  memory.count = getArraySize("memory blocks");
+  Object block = {0, NULL}, file = {0, NULL};
+  block.count = getArraySize("memory blocks");
   file.count = getArraySize("files");
-  memory.sizes = getArrayElements("memory blocks", "block", memory.count);
+  block.sizes = getArrayElements("memory blocks", "block", block.count);
   file.sizes = getArrayElements("files", "file", file.count);
   Process *processes = (Process *)malloc(file.count * sizeof(Process));
 
-  bestFit(memory, file, processes);
-  printOutput(memory, file, processes);
+  bestFit(block, file, processes);
+  printOutput(file.count, processes);
+  free(block.sizes);
+  free(file.sizes);
+  free(processes);
 
   return 0;
 }
